0b1d1bb700127a39c827ad638d1bd4cdfb108539
k-ankit <nvsankit2020@gmail.com>
2024-10-15 00:21:59 +0530

    feat: added comparison operators

--- include/big_int.h
+++ include/big_int.h
@@ -1,48 +1,126 @@
 #pragma once
 #ifndef BIGINT_H
 #define BIGINT_H
-#include<iostream>
-#include<string>
-#include <algorithm>  // For std::all_of
-#include <cctype>     // For std::isdigit
+#include <iostream>
+#include <string>
+#include <algorithm> // For std::all_of
+#include <cctype>    // For std::isdigit
+
+class Big_int
+{
+    std::string num; // private string which will hold the given value. in case of negative no num[0]= '-'
+
+    // functions for internal use of the class----
+
+    bool is_valid_num(std::string);
+    static bool is_valid_number(const std::string &input);
+
+    static bool is_strictly_maximum(const std::string, const std::string);
+    static bool is_loosely_maximum(const std::string, const std::string);
+    
+    static bool is_strictly_minimum(const std::string, const std::string);
+    static bool is_loosely_minimum(const std::string, const std::string);
+
+    static bool is_equal(const std::string, const std::string);
+   
 
 
-class Big_int{
-    std::string num;// private string which will hold the given value. in case of negative no num[0]= '-'
-     bool is_valid_num(std::string );
-    static bool is_valid_number(const std::string& input);  
-    public:  
-        Big_int(): num("0"){};//default value assigned expected to be 0
-        Big_int(int); // if int used
-        Big_int(long long int); 
-        Big_int(double );
-        Big_int(float );
-        Big_int(std::string);//will assign the valid input number
-       void print();
-       // Friend function to overload << operator for output
-    friend std::ostream& operator<<(std::ostream& stream, const Big_int& n);
-    //input stream
-    friend std::istream& operator>>(std::istream& stream, Big_int& n);
+public:
+    Big_int() : num("0") {}; // default value assigned expected to be 0
+    Big_int(int);            // if int used
+    Big_int(long long int);
+    Big_int(double);
+    Big_int(float);
+    Big_int(std::string); // will assign the valid input number
+    void print();
+    static Big_int to_big_int(const auto &value)
+    {
+        return Big_int(std::to_string(value));
+    }
 
-   // Friend function for prefix increment
-    friend Big_int& operator++(Big_int& n);
-   // Friend function for prefix decrement
-    friend Big_int& operator--(Big_int& n);
+    friend std::ostream &operator<<(std::ostream &stream, const Big_int &n); // Friend function to overload << operator for output
+    friend std::istream &operator>>(std::istream &stream, Big_int &n);       // input stream
+    friend Big_int &operator++(Big_int &n);                                  // Friend function for prefix increment
+    friend Big_int &operator--(Big_int &n);                                  // Friend function for prefix decrement
+    friend Big_int operator++(Big_int &n, int);                              // Postfix increment operator (with int parameter to differentiate it)
+    friend Big_int operator--(Big_int &n, int);                              // Postfix decrement operator (with int parameter to differentiate it)
 
-     // Postfix increment operator (with int parameter to differentiate it)
-    friend Big_int operator++(Big_int& n, int);
-     // Postfix decrement operator (with int parameter to differentiate it)
-    friend Big_int operator--(Big_int& n, int);
+    /*
+        ========================================================================
+        -----------------------------Arithmetic operators------------------------
+        =========================================================================
 
-    //Operator for Sum of two BigInt 
-    friend Big_int operator+(const Big_int& n1, const Big_int& n2);
+    */
+    friend Big_int operator+(const Big_int &n1, const Big_int &n2); // Operator for Sum of two BigInt
+    friend Big_int operator-(const Big_int &n1, const Big_int &n2); // Operator for  difference of two BigInt
 
-    // Operator for  difference of two BigInt
-    friend Big_int operator-(const Big_int& n1, const Big_int& n2);
+    /*
+        ==================================================================================
+       --------------------------------comparison operators-------------------------------
+        ==================================================================================
+    */
+    
+    template <typename T>
+    friend bool operator>(const T &n1, const Big_int &n2){
+        Big_int bn1 = to_big_int(n1);
+        return is_strictly_maximum(bn1.num, n2.num);
+    }
+
+    friend bool operator>(const Big_int &n1, const Big_int &n2){
+        return is_strictly_maximum(n1.num, n2.num);
+    }
+
+    template <typename T>
+    friend bool operator>=(const T &n1, const Big_int &n2){
+        Big_int bn1 = to_big_int(n1);
+        return is_loosely_maximum(bn1.num, n2.num);
+    }
+
+    friend bool operator>=(const Big_int &n1, const Big_int &n2){
+        return is_loosely_maximum(n1.num, n2.num);
+    }
+
+    template <typename T>
+    friend bool operator<(const T &n1, const Big_int &n2){
+        Big_int bn1 = to_big_int(n1);
+        return is_strictly_minimum(bn1.num, n2.num);
+    }
+
+    friend bool operator<(const Big_int &n1, const Big_int &n2){
+        return is_strictly_minimum(n1.num, n2.num);
+    }
+
+    template <typename T>
+    friend bool operator<=(const T &n1, const Big_int &n2){
+        Big_int bn1 = to_big_int(n1);
+        return is_loosely_minimum(bn1.num, n2.num);
+    }
+
+    friend bool operator<=(const Big_int &n1, const Big_int &n2){
+        return is_loosely_minimum(n1.num, n2.num);
+    }
+
+    template <typename T>
+    friend bool operator==(const T &n1, const Big_int &n2){
+        Big_int bn1= to_big_int(n1);
+        return is_equal(bn1.num, n2.num);
+    }
+
+    friend bool operator==(const Big_int &n1, const Big_int &n2){
+        return is_equal(n1.num, n2.num);
+    }
+
+    template <typename T>
+    friend bool operator!=(const T &n1, const Big_int &n2){
+       return !(n1 == n2);
+    }
+
+    friend bool operator!=(const Big_int &n1, const Big_int &n2){
+        return !(n1 == n2);
+    }
 
 
 
 };
-
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,62 @@
 #include "../include/big_int.h"
 #include <iostream>
 
-int main() {
+int main()
+{
     Big_int number1;
     Big_int number2;
     Big_int number3;
 
-    try {
-    std::cout << "Enter a valid number1: ";
-        std::cin >> number1;  // Input a number
-    std::cout << "Enter a valid number2: ";
-        std::cin >> number2;  // Input a number
-    std::cout << "Enter a valid number3: ";
-        std::cin >> number3;  // Input a number
-      // Print the number if valid
-        // std::cout<<--number<<"\n";
-        std::cout<<number1-number2<<"\n";
+    // try
+    // {
+    //     std::cout << "Enter a valid number1: ";
+    //     std::cin >> number1; // Input a number
+    //     std::cout << "Enter a valid number2: ";
+    //     std::cin >> number2; // Input a number
+    //     std::cout << "Enter a valid number3: ";
+    //     std::cin >> number3; // Input a number
+    //                          // Print the number if valid
+    //     // std::cout<<--number<<"\n";
+    //     std::cout << number1 - number2 << "\n";
 
-        // std::cout<<++number<<"\n";
-        // std::cout<<number--<<"\n";
-        // std::cout<<number<<"\n";
-            // Print the number if valid
-    } catch (const std::invalid_argument& e) {
-        std::cerr << e.what() << std::endl;  // Handle invalid input
+    //     // std::cout<<++number<<"\n";
+    //     // std::cout<<number--<<"\n";
+    //     // std::cout<<number<<"\n";
+    //     // Print the number if valid
+    // }
+    // catch (const std::invalid_argument &e)
+    // {
+    //     std::cerr << e.what() << std::endl; // Handle invalid input
+    // }
+
+    /*
+        ============================================================
+        =================condtional operators check=================
+        ============================================================
+    */
+
+    try
+    {
+        // Big_int num1;
+        // Big_int num2;
+        // std::cin >> num1 >> num2;
+        long long num2= 100;
+         Big_int num1("100");
+        std::cout<<num1<<"\n";
+        bool res = (num1 >= num2);
+        std::cout << " greater than comparison result :: " << res << std::endl;
+        bool res1 = (num1 <= num2);
+        std::cout << " less than comparison result :: " << res1 << std::endl;
+
+        Big_int n1("1234567898");
+        Big_int n2("1234567898");
+        std::cout<<"equal comparison result :: "<< (n1 == n2)<<std::endl;
+        std::cout<<"Not Equal comparison result :: "<< (n1 != n2)<<std::endl;
     }
-    
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     return 0;
 }
